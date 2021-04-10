2aa33962bbb95e746bcbfa97419d6da46f922c97
Yeison Stiven Builes Uribe <yeisonplox@gmail.com>
2021-04-09 21:53:39 -0500

    Credit card done

--- 002_Credit_Card/lib/index.js
+++ 002_Credit_Card/lib/index.js
@@ -3,13 +3,57 @@
 var cardNumberInfo = document.querySelector(".number-info");
 var cardExpiryInfo = document.querySelector(".expiry-info");
 var cardNameInfo = document.querySelector(".name-info");
-var inputs = document.querySelectorAll("input"); //npm run build continuar con la actualizacion en directo de la tarjeta, agregar validaciones a los inputs, subir a git
-
+var inputs = document.querySelectorAll("input");
+var cdType = document.querySelector(".cd-type");
+var advice = document.querySelector(".advice");
+var modal = document.querySelector(".modalize");
+var modalCta = document.querySelector(".modal-cta");
+var btnProceed = document.querySelector(".btn");
+var creditCardInfo = {
+  number: [],
+  expiry: "",
+  holder: "",
+  cvv: ""
+};
+var mastercardStartWith = ["51", "52", "53", "54", "55", "222100-272099"];
 inputs.forEach(function (input) {
   input.addEventListener('input', function () {
     switch (input.id) {
       case "card_number":
         cardNumberInfo.innerHTML = input.value;
+
+        if (cardNumberInfo.textContent.startsWith("4")
+        /*&&cardNumberInfo.textContent.length>=13*/
+        ) {
+            cdType.innerHTML = "<i class='cd-type bx bxl-visa' ></i>";
+          } else {
+          cdType.innerHTML = "<i class='bx bx-credit-card-front' ></i>";
+        }
+
+        mastercardStartWith.forEach(function (n) {
+          if (cardNumberInfo.textContent.startsWith(n)) {
+            cdType.innerHTML = "<i class='cd-type bx bxl-mastercard' ></i> MasterCard";
+          }
+        }); //validation
+
+        if (cardNumberInfo.textContent.length < 13) {
+          advice.innerHTML = "Must be more than 13 characters";
+        } else {
+          advice.innerHTML = "";
+        }
+
+        if (cardNumberInfo.textContent.length === 0) {
+          advice.innerHTML = "";
+        }
+
+        break;
+
+      case "card_expiry":
+        cardExpiryInfo.innerHTML = input.value;
+        break;
+
+      case "card_name":
+        cardNameInfo.innerHTML = input.value.toUpperCase();
         break;
 
       default:
@@ -17,4 +61,10 @@ inputs.forEach(function (input) {
         break;
     }
   });
-}); //console.log(inputs)
+});
+btnProceed.addEventListener("click", function () {
+  return modal.style.display = "inherit";
+});
+modalCta.addEventListener("click", function () {
+  return modal.style.display = "none";
+});
--- 002_Credit_Card/src/index.js
+++ 002_Credit_Card/src/index.js
@@ -1,20 +1,58 @@
 const cardNumberInfo = document.querySelector(".number-info");
 const cardExpiryInfo = document.querySelector(".expiry-info");
-const cardNameInfo = document.querySelector(".name-info")
-const inputs = document.querySelectorAll("input")
+const cardNameInfo = document.querySelector(".name-info");
+const inputs = document.querySelectorAll("input");
+const cdType = document.querySelector(".cd-type");
+const advice = document.querySelector(".advice");
+const modal = document.querySelector(".modalize");
+const modalCta = document.querySelector(".modal-cta")
+const btnProceed = document.querySelector(".btn");
+let creditCardInfo = {
+    number: [],
+    expiry: "",
+    holder:"",
+    cvv:""
+}
+const mastercardStartWith = ["51","52","53","54","55","222100-272099"]
 
-//npm run build continuar con la actualizacion en directo de la tarjeta, agregar validaciones a los inputs, subir a git
 inputs.forEach((input)=>{
     input.addEventListener('input',()=>{
         switch(input.id){
             case "card_number":
                 cardNumberInfo.innerHTML = input.value
+                if(cardNumberInfo.textContent.startsWith("4")/*&&cardNumberInfo.textContent.length>=13*/){
+                    cdType.innerHTML = "<i class='cd-type bx bxl-visa' ></i>"
+                }else{
+                    cdType.innerHTML = "<i class='bx bx-credit-card-front' ></i>"
+                }
+                mastercardStartWith.forEach(n=>{
+                    if(cardNumberInfo.textContent.startsWith(n)){
+                        cdType.innerHTML = "<i class='cd-type bx bxl-mastercard' ></i> MasterCard"
+                    }
+                });
+
+                //validation
+                if(cardNumberInfo.textContent.length<13){
+                    advice.innerHTML = "Must be more than 13 characters";
+                }else{
+                    advice.innerHTML = "";
+                }
+                if(cardNumberInfo.textContent.length === 0){
+                    advice.innerHTML = "";
+                }
                 break;
+            case "card_expiry":
+                cardExpiryInfo.innerHTML = input.value
+                break
+            case "card_name":
+                cardNameInfo.innerHTML =  input.value.toUpperCase()
+                break
             default:
                 console.log("nop");
                 break;
         }
     });
-
 });
-//console.log(inputs)
+
+btnProceed.addEventListener("click",()=>modal.style.display = "inherit");
+modalCta.addEventListener("click",() => modal.style.display = "none");
