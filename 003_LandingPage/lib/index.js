c2c1ac304677fcc803f5651b97ea18a7225590f7
Yeison Stiven Builes Uribe <yeisonplox@gmail.com>
2021-04-13 18:12:02 -0500

    header naimated with mouse movement adn first section created

--- 003_LandingPage/lib/index.js
+++ 003_LandingPage/lib/index.js
@@ -1,4 +1,11 @@
 "use strict";
 
-var prueba = "ola".concat(1 + 1);
-console.log("prueba");
+var header = document.getElementById("header");
+header.addEventListener("mousemove", function (evt) {
+  document.querySelectorAll(".layer").forEach(function (element) {
+    var speed = element.getAttribute("data-speed");
+    var x = (window.innerWidth - evt.pageX * speed) / 100;
+    var y = (window.innerHeight - evt.pageY * speed) / 100;
+    element.style.transform = "translateX(".concat(x, "px) translateY(").concat(y, "px)");
+  });
+});
--- 003_LandingPage/src/index.js
+++ 003_LandingPage/src/index.js
@@ -1,2 +1,11 @@
-let prueba = `ola${1+1}`
-console.log("prueba")
+const header = document.getElementById("header")
+
+header.addEventListener("mousemove",(evt)=>{
+    document.querySelectorAll(".layer").forEach(element => {
+        const speed = element.getAttribute("data-speed")
+        const x = (window.innerWidth - evt.pageX*speed)/100
+        const y = (window.innerHeight - evt.pageY*speed)/100
+
+        element.style.transform = `translateX(${x}px) translateY(${y}px)`
+    });
+});
