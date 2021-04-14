e3d6928190499b28d612d641e34c67b024306475
Yeison Stiven Builes Uribe <yeisonplox@gmail.com>
2021-04-14 16:17:50 -0500

    dark mode logic created

--- 003_LandingPage/lib/index.js
+++ 003_LandingPage/lib/index.js
@@ -5,13 +5,26 @@ var aboutImg = document.querySelector(".about-img");
 var about = document.querySelector(".about-content");
 var darkModeSwitch = document.querySelector(".dark-mode-switch");
 var darkModeIcon = document.querySelector(".bx-dark-mode");
+var emailInput = document.querySelector("#email");
 darkModeSwitch.addEventListener("input", function (evy) {
   if (darkModeSwitch.checked) {
     darkModeIcon.classList.add("bxs-sun");
     darkModeIcon.classList.remove("bxs-moon");
+    emailInput.style.color = "#FBFEFF";
+    document.querySelector("#main").style.backgroundColor = "#03080A";
+    document.querySelectorAll(".text").forEach(function (text) {
+      text.style.color = "#FBFEFF";
+      console.log(text);
+    });
   } else {
     darkModeIcon.classList.remove("bxs-sun");
     darkModeIcon.classList.add("bxs-moon");
+    emailInput.style.color = "#03080A";
+    document.querySelector("#main").style.backgroundColor = "#FBFEFF";
+    document.querySelectorAll(".text").forEach(function (text) {
+      text.style.color = "#03080A";
+      console.log(text);
+    });
   }
 });
 header.addEventListener("mousemove", function (evt) {
--- 003_LandingPage/src/index.js
+++ 003_LandingPage/src/index.js
@@ -3,14 +3,27 @@ const aboutImg = document.querySelector(".about-img");
 const about = document.querySelector(".about-content");
 const darkModeSwitch = document.querySelector(".dark-mode-switch");
 const darkModeIcon = document.querySelector(".bx-dark-mode");
+const emailInput = document.querySelector("#email");
 
 darkModeSwitch.addEventListener("input",evy=>{
     if(darkModeSwitch.checked){
         darkModeIcon.classList.add("bxs-sun")
         darkModeIcon.classList.remove("bxs-moon")
+        emailInput.style.color = "#FBFEFF"
+        document.querySelector("#main").style.backgroundColor = "#03080A"
+        document.querySelectorAll(".text").forEach(text=>{
+            text.style.color = "#FBFEFF"
+            console.log(text)
+        })
     }else{
         darkModeIcon.classList.remove("bxs-sun")
         darkModeIcon.classList.add("bxs-moon")
+        emailInput.style.color = "#03080A"
+        document.querySelector("#main").style.backgroundColor = "#FBFEFF"
+        document.querySelectorAll(".text").forEach(text=>{
+            text.style.color = "#03080A"
+            console.log(text)
+        })
     }
 })
 
