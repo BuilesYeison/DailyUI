464f6b026c7431378d37f7b67a0c9f9ccf357569
Yeison Stiven Builes Uribe <yeisonplox@gmail.com>
2021-04-13 20:12:25 -0500

    switch icon in dark mode, aos implemented

--- 003_LandingPage/lib/index.js
+++ 003_LandingPage/lib/index.js
@@ -1,6 +1,19 @@
 "use strict";
 
 var header = document.getElementById("header");
+var aboutImg = document.querySelector(".about-img");
+var about = document.querySelector(".about-content");
+var darkModeSwitch = document.querySelector(".dark-mode-switch");
+var darkModeIcon = document.querySelector(".bx-dark-mode");
+darkModeSwitch.addEventListener("input", function (evy) {
+  if (darkModeSwitch.checked) {
+    darkModeIcon.classList.add("bxs-sun");
+    darkModeIcon.classList.remove("bxs-moon");
+  } else {
+    darkModeIcon.classList.remove("bxs-sun");
+    darkModeIcon.classList.add("bxs-moon");
+  }
+});
 header.addEventListener("mousemove", function (evt) {
   document.querySelectorAll(".layer").forEach(function (element) {
     var speed = element.getAttribute("data-speed");
@@ -8,4 +21,10 @@ header.addEventListener("mousemove", function (evt) {
     var y = (window.innerHeight - evt.pageY * speed) / 100;
     element.style.transform = "translateX(".concat(x, "px) translateY(").concat(y, "px)");
   });
+});
+about.addEventListener("mousemove", function (evt) {
+  var speed = aboutImg.getAttribute("data-speed");
+  var x = (window.innerWidth - evt.pageX * speed) / 100;
+  var y = (window.innerHeight - evt.pageY * speed) / 100;
+  aboutImg.style.transform = "translateX(".concat(x, "px) translateY(").concat(y, "px)");
 });
--- 003_LandingPage/src/index.js
+++ 003_LandingPage/src/index.js
@@ -1,11 +1,32 @@
-const header = document.getElementById("header")
+const header = document.getElementById("header");
+const aboutImg = document.querySelector(".about-img");
+const about = document.querySelector(".about-content");
+const darkModeSwitch = document.querySelector(".dark-mode-switch");
+const darkModeIcon = document.querySelector(".bx-dark-mode");
 
-header.addEventListener("mousemove",(evt)=>{
+darkModeSwitch.addEventListener("input",evy=>{
+    if(darkModeSwitch.checked){
+        darkModeIcon.classList.add("bxs-sun")
+        darkModeIcon.classList.remove("bxs-moon")
+    }else{
+        darkModeIcon.classList.remove("bxs-sun")
+        darkModeIcon.classList.add("bxs-moon")
+    }
+})
+
+header.addEventListener("mousemove", evt => {
     document.querySelectorAll(".layer").forEach(element => {
         const speed = element.getAttribute("data-speed")
-        const x = (window.innerWidth - evt.pageX*speed)/100
-        const y = (window.innerHeight - evt.pageY*speed)/100
+        const x = (window.innerWidth - evt.pageX * speed) / 100
+        const y = (window.innerHeight - evt.pageY * speed) / 100
 
         element.style.transform = `translateX(${x}px) translateY(${y}px)`
     });
 });
+
+about.addEventListener("mousemove", evt => {
+    const speed = aboutImg.getAttribute("data-speed")
+    const x = (window.innerWidth - evt.pageX * speed) / 100
+    const y = (window.innerHeight - evt.pageY * speed) / 100
+
+    aboutImg.style.transform = `translateX(${x}px) translateY(${y}px)`})
